441df77614509e97667ac1c4df5af0df4639b812
AVL <avl@larikhin.ru>
2022-02-02 18:09:32 +0300

    add comments in func_s.py in make_dicts_from_temp_list

--- func_s.py
+++ func_s.py
@@ -75,6 +75,16 @@ def make_clear_dicts():
 
 
 def make_dicts_from_temp_list(temp_list, srok_postavki, dir, file):
+    '''
+    - Создаем словари {ключи - парки (ap1), значения - списки [документ, дата, кол-во...]}
+    - Запускаем функцию poisk_v_dictax для поиска просрочек
+    - Запускаем функцию make_list_from_dict для перекладывания списков в словари
+    :param temp_list: список документов (исходные строки) по одной позиции спецификации
+    :param srok_postavki: и так понятно
+    :param dir: папка для сохранения исходных данных (номер договора) АТАВИЗМ
+    :param file: номер позиции спецификации АТАВИЗМ
+    :return: список просрочек
+    '''
     svodnaya_dict, postavka_dict, korrektirovka_dict = make_clear_dicts()
  #   write_csv(temp_list, dir, file)    # Для проверки можно писать исходные данные в файл
                                         #   (путь - /data_in/номер_договора/номер_позиции.csv)
@@ -82,18 +92,26 @@ def make_dicts_from_temp_list(temp_list, srok_postavki, dir, file):
         if 'Сводная' in row[0]:
             data_sv = datetime.strptime(row[0].split()[6], '%d.%m.%Y').date()
             for column in svodnaya_col_names:
+                # svodnaya_col_names - словарь {raz_ap1: номер столбца}
                 col = svodnaya_col_names[column]
                 if row[col] != '':
                     sv_value = int(row[col])
+                    # создаем временный лист для сводных
+                    # [Название документа, дата, кол-во, 'не закрыта Х дней']
                     sv_temp_list = [row[0], data_sv, sv_value, f'not closed: {srok_postavki_fact(current_date, data_sv)} days']
+                    # добавляем список в словарь по имени парка {ap1: [[],[]]}
                     svodnaya_dict[column[4:]].append(sv_temp_list)
         elif 'Приобретение' in row[0]:
             data_pr = datetime.strptime(row[0].split()[6], '%d.%m.%Y').date()
             for column in postavka_col_names:
+                # postavka_col_names - словарь {pos_ap1: номер столбца}
                 col = postavka_col_names[column]
                 if row[col] != '':
                     pr_value = int(row[col])
+                    # создаем временный лист для приобретений
+                    # [Название документа, дата, кол-во]
                     pr_temp_list = [row[0], data_pr, pr_value]
+                    # добавляем список в словарь по имени парка {ap1: [[],[]]}
                     postavka_dict[column[4:]].append(pr_temp_list)
         elif 'Корректировка' in row[0]:
             data_pr = datetime.strptime(row[0].split()[7], '%d.%m.%Y').date()
@@ -101,7 +119,10 @@ def make_dicts_from_temp_list(temp_list, srok_postavki, dir, file):
                 col = svodnaya_col_names[column]
                 if row[col] != '':
                     pr_value = int(row[col])
+                    # создаем временный лист для корректировок
+                    # [Название документа, дата, кол-во]
                     pr_temp_list = [row[0], data_pr, pr_value]
+                    # добавляем список в словарь ПОСТАВОК по имени парка {ap1: [[],[]]}
                     postavka_dict[column[4:]].append(pr_temp_list)
     x = poisk_v_dictax(svodnaya_dict, postavka_dict, srok_postavki)
     some_list = make_list_from_dict(x)
